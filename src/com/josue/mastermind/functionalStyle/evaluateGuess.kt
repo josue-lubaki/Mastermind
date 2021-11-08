c04d0af816519e66067e1b0594c3031eef9603fe
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-11-08 05:00:24 -0500

    add functional style

---
+++ src/com/josue/mastermind/functionalStyle/evaluateGuess.kt
@@ -0,0 +1,25 @@
+package com.josue.mastermind.functionalStyle
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int) {
+    infix fun eq(result: Evaluation) {
+        if (this == result) println("OK")
+    }
+}
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
+
+    val commonLetters = "ABCDEF".sumOf { ch ->
+
+        Math.min(secret.count { it == ch }, guess.count { it == ch })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
+}
+
+fun main(args: Array<String>) {
+    val result = Evaluation(rightPosition = 1, wrongPosition = 1)
+    evaluateGuess("BCDF", "ACEB") eq result
+    evaluateGuess("AAAF", "ABCA") eq result
+    evaluateGuess("ABCA", "AAAF") eq result
+}
